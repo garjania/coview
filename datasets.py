f7c4b79879be4510fd1df56f53d269e848cdba2b
Ali Garjani <garjani.a@gmail.com>
2025-02-01 20:09:30 +0000

    Add SequentialVideoDataset

--- datasets.py
+++ datasets.py
@@ -1,3 +1,4 @@
+import torch
 from torchvision.datasets import ImageFolder
 
 def frame_separator(frame_path):
@@ -26,3 +27,46 @@ class SimpleVideoDataset(ImageFolder):
             
         return image, frame_id
     
+class SequentialVideoDataset(ImageFolder):
+    def __init__(self, root, frame_len=16, index_slice=None, transform=None):
+        # Call parent class constructor with root directory and transform
+        super().__init__(
+            root=root,
+            transform=transform,
+            target_transform=None,
+        )
+        self.frame_len = frame_len
+        
+        # Sort samples by frame number and optionally slice
+        self.samples = sorted(self.samples, key=lambda x: frame_separator(x[0]))
+        if index_slice is not None:
+            self.samples = self.samples[index_slice[0]:index_slice[1]]
+            
+    def __getitem__(self, idx):
+        # Ensure we don't go out of bounds
+        if idx + self.frame_len > len(self.samples):
+            raise IndexError("Sequence would extend beyond dataset bounds")
+            
+        frames = []
+        frame_ids = []
+        
+        # Get the sequence of frames
+        for i in range(self.frame_len):
+            path = self.samples[idx + i][0]
+            image = self.loader(path)
+            frame_id = frame_separator(path)
+            
+            if self.transform is not None:
+                image = self.transform(image)
+                
+            frames.append(image)
+            frame_ids.append(frame_id)
+            
+        # Stack frames along a new dimension
+        frames = torch.stack(frames)
+        frame_ids = torch.tensor(frame_ids)
+        
+        return frames, frame_ids
+        
+    def __len__(self):
+        return max(0, len(self.samples) - self.frame_len + 1)
